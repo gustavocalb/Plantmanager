77bf1b2359a3441bb2878f9963b8e59ab6583224
FireShark688 <gugacalb@gmail.com>
2021-05-15 18:40:04 -0300

    .

---
+++ src/components/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  Image
+} from 'react-native';
+
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import userImg from '../assets/gustavo.png'
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts'
+
+
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.username}>Gustavo</Text>
+      </View>
+      <Image source={userImg} style={styles.image}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  username: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  }
+})
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+
+import StackRoute from './stack.routes'
+
+
+const Routes = () => (
+  <NavigationContainer>
+    <StackRoute />
+  </NavigationContainer>
+)
+
+export default Routes
